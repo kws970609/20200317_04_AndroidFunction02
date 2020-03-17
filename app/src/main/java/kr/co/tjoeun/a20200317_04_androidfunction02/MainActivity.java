a861b95a13899cf9b78fbf45b2e04eb9b9263a5b
kws970609 <kws970609@users.noreply.github.com>
2020-03-17 17:16:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200317_04_androidfunction02/MainActivity.java
@@ -0,0 +1,39 @@
+package kr.co.tjoeun.a20200317_04_androidfunction02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import kr.co.tjoeun.a20200317_04_androidfunction02.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.dialBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                다른 화면으로 이동 : Intent
+//                => 우리가 만든게 아니라, 안드로이드가 제공하는 화면으로 이동.
+//                 전화 걸기화면
+
+//                어느화면으로 갈지 => 안드로이드가 요구하는 양식으로 작성 : Uri
+                Uri uri = Uri.parse("tel:01045678965");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
