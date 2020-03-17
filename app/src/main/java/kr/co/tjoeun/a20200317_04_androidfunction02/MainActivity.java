a652d9071ff6ecd74c93948062df455bd7355f87
kws970609 <kws970609@users.noreply.github.com>
2020-03-17 17:51:50 +0900

    문자 보내기 코드

--- app/src/main/java/kr/co/tjoeun/a20200317_04_androidfunction02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_04_androidfunction02/MainActivity.java
@@ -55,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("smsto:12345678");
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra("sms_body", "공유 메세지 내용");
+                startActivity(intent);
+            }
+        });
+
 
 
     }
